f7ce57ae66d40aa447af748554f2db1ecab7ba9e
Vityaz-XII <setiawan2206>
2024-01-01 21:32:23 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function registerPost(Request $request)
+    {
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return back()->with('succcess', 'Register Successfully');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+        if (Auth::attempt($credentials)) {
+            session()->flash('is_logged_in', true);
+            return redirect()->intended('home')->with('success', 'Login Berhasil');
+        }
+        return back()->with('error', 'Wrong Email or Password !');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login');
+    }
+
+}
